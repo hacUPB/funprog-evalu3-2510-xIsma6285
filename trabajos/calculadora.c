d841de497564594979df0753d54ad2ac5a815fe7
Ismael Aristizabal <ismael.aristizabal@upb.edu.co>
2025-03-11 14:37:39 +0000

    Se agrega codigo de la calculadora

---
+++ trabajos/calculadora.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    int operacion;
+
+    printf("1. Suma\n, Resta\n2, Multiplicacion\n3, Division\n4")
+
+    scanf("&d",&operacion)
+
+}
